366d8eb6e2738dabc3adbce91cdb85446c0666f5
Abud Zohair <abud.zohair@gmail.com>
2020-10-06 14:43:07 +0200

    add post model with timestamp

--- models/postModel.js
+++ models/postModel.js
@@ -1,13 +1,23 @@
 const mongoose = require('mongoose');
-const postSchema = new mongoose.Schema({
-  caption: String,
-  images: [
-    {
+const postSchema = new mongoose.Schema(
+  {
+    caption: {
       type: String,
-      required: [true, 'A post must have images']
-    }
-  ]
-});
+      required: true
+    },
+    images: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'images',
+      required: true
+    },
+    isAnonymous: {
+      type: Boolean,
+      default: false
+    },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'user' }
+  },
+  { timestamps: true }
+);
 
 const Post = mongoose.model('Post', postSchema);
 module.exports = Post;
