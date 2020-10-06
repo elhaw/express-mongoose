6a8863ca10a997e50d2514d19cc5b7347ab58dc2
Abud Zohair <abud.zohair@gmail.com>
2020-10-06 12:36:54 +0200

    Clean up imports

--- models/postModel.js
+++ models/postModel.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
-const Image  = require('./imageModel')
-const postSchema = new mongoose.Schema(
-  {
-    caption: String,
-    images: [{
+const postSchema = new mongoose.Schema({
+  caption: String,
+  images: [
+    {
       type: String,
       required: [true, 'A post must have images']
-    }],
-  },
-);
+    }
+  ]
+});
 
-const Post = mongoose.model("Post", postSchema);
+const Post = mongoose.model('Post', postSchema);
 module.exports = Post;
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,23 +1,13 @@
 const express = require('express');
 const postController = require('../controllers/postController');
-const imageRouter = require('./imageRoutes');
 
 const router = express.Router();
 
-router.use('/:postId/images', imageRouter);
+router.route('/').post(postController.createPost);
 
 router
-  .route('/')
-  .post(
-    postController.createPost
-  );
-
-  router
   .route('/:id')
   .get(postController.getPost)
-  .patch(
-    postController.uploadImages,
-    postController.updatePost
-  )
+  .patch(postController.uploadImages, postController.updatePost);
 
 module.exports = router;
